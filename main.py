f04557c33c2624f7214d6a188121357418854629
Guilherme Lopes Borges <med.gborges@gmail.com>
2024-08-28 21:30:37 +0200

    [FEATURE] Load phantom image

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ from scipy.interpolate import RectBivariateSpline
 from skimage.data import shepp_logan_phantom
 from skimage.transform import radon, rescale, rotate
 
+image = shepp_logan_phantom()
 image = np.ones([100,100])

6f0ead3b24c2c7da2147e178d0f0f2f4a892c498
Guilherme Lopes Borges <med.gborges@gmail.com>
2024-08-28 22:58:07 +0200

    [FEATURE] Extend to use skimage.transform radon,iradon

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ from scipy.interpolate import RectBivariateSpline
 from skimage.data import shepp_logan_phantom
 from skimage.transform import radon, rescale, rotate
 
-# image = shepp_logan_phantom()
-image = np.ones([100,100])
+image = shepp_logan_phantom()
+# image = np.ones([100,100])
 # Resize Image
-diag = len(np.diag(image)//2)
-image = np.pad(image, pad_width=diag+10)
+# diag = len(np.diag(image)//2)
+# image = np.pad(image, pad_width=diag+10)
 
 _ = np.linspace(-1, 1, image.shape[0])
 xv, yv = np.meshgrid(_,_)
-image[(xv-0.1)**2+(yv-0.2)**2<0.01] = 2
+# image[(xv-0.1)**2+(yv-0.2)**2<0.01] = 2
 
 # At this point, the image as values 0 background,1 square,2 circle. Pretending to represent x-ray attenuation.
 
@@ -36,7 +36,7 @@ ax[0].pcolor(xv,yv,image, shading='auto')
 ax[1].pcolor(xv,yv,image_rot, shading='auto')
 plt.show()
 
-
+# Change step angle to 1° for better results, calculation will be slower
 thetas = np.arange(0,180,5) * np.pi/180 # Rotations, from 0 to 180°, every 5°, then converts to radians.
 rs = _
 dtheta = np.diff(thetas)[0] # the difference between the data in the array
@@ -85,6 +85,8 @@ integrand = P.T * np.abs(nu)
 integrand = integrand.T
 p_p = np.real(ifft(integrand, axis=0))  # p'
 
+# plt.imshow(p_p)
+
 p_p_interp = RectBivariateSpline(rs, thetas, p_p)
 
 def get_f(x,y):
@@ -96,3 +98,22 @@ plt.figure(figsize=(6,6))
 plt.pcolor(f)
 plt.show()
 
+plt.plot(f[110])
+plt.show()
+
+from skimage.transform import radon, iradon
+
+theta = np.arange(0., 180., 5)
+sinogram = radon(image, theta=theta)
+
+plt.pcolor(sinogram)
+plt.show()
+
+reconstruction_img = iradon(sinogram, theta=theta, filter_name='ramp')
+
+plt.figure(figsize=(6,6))
+plt.pcolor(reconstruction_img)
+plt.show()
+
+plt.plot(reconstruction_img[110])
+plt.show()

8916820dd4ecdb3ec4036af624a336fbf3848dc6
Guilherme Lopes Borges <med.gborges@gmail.com>
2024-08-28 22:38:30 +0200

    [FEATURE] Implement regular and filtered back projection

--- main.py
+++ main.py
@@ -1,11 +1,98 @@
 # To create the file with installed libraries: pip freeze > requirements.txt
 
 import numpy as np
+# import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use('TkAgg')  # Or 'Qt5Agg' or 'Agg' for non-interactive environments
 import matplotlib.pyplot as plt
+
+# Enable interactive mode
+# plt.ion()
 
 from scipy.interpolate import RectBivariateSpline
 from skimage.data import shepp_logan_phantom
 from skimage.transform import radon, rescale, rotate
 
-image = shepp_logan_phantom()
+# image = shepp_logan_phantom()
 image = np.ones([100,100])
+# Resize Image
+diag = len(np.diag(image)//2)
+image = np.pad(image, pad_width=diag+10)
+
+_ = np.linspace(-1, 1, image.shape[0])
+xv, yv = np.meshgrid(_,_)
+image[(xv-0.1)**2+(yv-0.2)**2<0.01] = 2
+
+# At this point, the image as values 0 background,1 square,2 circle. Pretending to represent x-ray attenuation.
+
+# The image(object) will be rotated, representing the scanner. It has the same effect.
+
+# Create a rotated image
+image_rot = rotate(image, 45)
+
+fig, ax = plt.subplots(1,2, figsize=(10,5))
+ax[0].pcolor(xv,yv,image, shading='auto')
+ax[1].pcolor(xv,yv,image_rot, shading='auto')
+plt.show()
+
+
+thetas = np.arange(0,180,5) * np.pi/180 # Rotations, from 0 to 180°, every 5°, then converts to radians.
+rs = _
+dtheta = np.diff(thetas)[0] # the difference between the data in the array
+dr = np.diff(rs)[0]
+rotations = np.array([rotate(image, theta*180/np.pi) for theta in thetas]) # 5°, 10°, 15° ....
+
+# plt.imshow(rotations[1])
+# plt.show()
+
+# Sum of the values in the vertical exis, p is a 2D data
+p = np.array([rotation.sum(axis=0)*dr for rotation in rotations]).T
+
+plt.plot(rs, p[:,9])
+plt.xlabel('r', fontsize=20)
+plt.ylabel('$\ln(I_0/I)$', fontsize=20)
+plt.show()
+
+
+plt.pcolor(thetas, rs, p, shading='auto')
+plt.xlabel(r'$\theta$', fontsize=20)
+plt.ylabel('$r$', fontsize=20)
+plt.show()
+
+
+p_interp = RectBivariateSpline(rs, thetas, p)
+
+def get_fBP(x,y):
+    return p_interp(x*np.cos(thetas)+y*np.sin(thetas), thetas, grid=False).sum() * dtheta
+
+fBP = np.vectorize(get_fBP)(xv,yv)
+
+plt.figure(figsize=(6,6))
+plt.pcolor(fBP)
+plt.show()
+
+from scipy.fft import fft, ifft
+
+P = fft(p, axis=0)
+nu = np.fft.fftfreq(P.shape[0], d=np.diff(rs)[0])
+
+P.T.shape
+
+nu.shape
+
+integrand = P.T * np.abs(nu)
+integrand = integrand.T
+p_p = np.real(ifft(integrand, axis=0))  # p'
+
+p_p_interp = RectBivariateSpline(rs, thetas, p_p)
+
+def get_f(x,y):
+    return p_p_interp(x*np.cos(thetas)+y*np.sin(thetas), thetas, grid=False).sum() * dtheta
+
+f = np.vectorize(get_f)(xv,yv)
+
+plt.figure(figsize=(6,6))
+plt.pcolor(f)
+plt.show()
+
